543b7eede97957e67589ae52a69c66218442c649
olsondeven <olsondeven@gmail.com>
2016-12-15 13:55:42 -0700

    added files

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var config = require('./config');
+// var con = massive.connectSync({
+//   connectionString : "postgres://massive:@localhost/your-database-name"
+// });
 
 var app = express();
 app.use(bodyParser.json());
-
-var port = 3000;
 
 app.get('/incidents', function(req, res) {
   console.log('POST sighting');
@@ -15,6 +18,6 @@ app.post('/incidents', function(req, res) {
   console.log('POST sighting');
 });
 
-app.listen(port, function() {
-  console.log("Started server on port", port);
+app.listen(config.port, function() {
+  console.log("Listening to port: ", config.port);
 });
